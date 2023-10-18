9335bdd6b9e648cacce3ded67a0d9cb7d0713ad9
Allen Chou <f1470891079@gmail.com>
2023-10-18 18:35:53 +0800

    Update typing-extensions version

--- flowter/__init__.py
+++ flowter/__init__.py
@@ -1,3 +1,28 @@
+import time
+from functools import wraps
+from typing import Callable, TypeVar
+
+from typing_extensions import ParamSpec
+
 from .version import VERSION
 
 __version__ = VERSION
+
+T = TypeVar("T")
+P = ParamSpec("P")
+
+
+class flow:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, func: Callable[P, T]) -> Callable[P, T]:
+        @wraps(func)
+        def wrapper(*inner_args: P.args, **inner_kwargs: P.kwargs) -> T:
+            start = time.time()
+            result = func(*inner_args, **inner_kwargs)
+            end = time.time()
+            print(f"{func.__name__} took {end-start:.2f} seconds to run.")
+            return result
+
+        return wrapper
